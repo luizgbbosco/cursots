d0cb86dbd22129828ef307cb4cb7c683acb39517
luizgbbosco <luizgbbosco@gmail.com>
2018-08-13 10:12:14 -0300

    adicionado novos exemplos nos arquivos

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
-// function listaReversa <T> (lista : Array<T>): Array<T> {
-//     let listaReversa: T[] = [];
-//         for (let i = lista.length -1; i>=0 ;i--){
-//             listaReversa.push(lista[i]);
-//         }
-//     return listaReversa;
-// }
-// let nomes = ["Luiz", "Marcos", "Lucas"];
-// let numeros = [1,2,3,4,5];
-// let nomesReversos = listaReversa<string>(nomes);
-// let numerosReversos = listaReversa<number>(numeros);
-// console.log(nomesReversos);
-// console.log(numerosReversos);
+function listaReversa(lista) {
+    var listaReversa = [];
+    for (var i = lista.length - 1; i >= 0; i--) {
+        listaReversa.push(lista[i]);
+    }
+    return listaReversa;
+}
+var nomes = ["Luiz", "Marcos", "Lucas"];
+var numeros = [1, 2, 3, 4, 5];
+var nomesReversos = listaReversa(nomes);
+var numerosReversos = listaReversa(numeros);
+console.log(nomesReversos);
+console.log(numerosReversos);
 var ClickHandler = /** @class */ (function () {
     function ClickHandler() {
         var main = document.getElementById("main");
@@ -27,13 +27,45 @@ var ClickHandler = /** @class */ (function () {
         event.stopPropagation();
     };
     ClickHandler.alertEventBtn1 = function () {
-        alert("Clicked over btn1");
+        alert("Você escolheu Goku");
         event.stopPropagation();
     };
     ClickHandler.alertEventBtn2 = function () {
-        alert("Clicked over btn2");
+        alert("Você escolheu Vegeta");
         event.stopPropagation();
     };
     return ClickHandler;
 }());
 var clickHandler = new ClickHandler();
+//     let button1 = document.querySelector("#btn1");
+//     let button2 = document.querySelector("#btn2");
+//     console.log(button1);
+//     console.log(button2);
+// var main2 = document.getElementById("main");
+// main2.innerHTML = "Alo"
+// console.log(main2);
+var session1 = "session1";
+var session2 = "session2";
+var session3 = "session3";
+sessionStorage.setItem(session1, "Luiz");
+sessionStorage.setItem(session2, "Gustavo");
+sessionStorage.setItem(session3, "Borges Bosco");
+console.log(sessionStorage.getItem(session1));
+console.log(sessionStorage.getItem(session2));
+console.log(sessionStorage.getItem(session3));
+console.log("------------------------------------");
+for (var i = 0; i < sessionStorage.length; i++) {
+    console.log(sessionStorage.getItem(sessionStorage.key(i)));
+}
+console.log("------------------------------------");
+var session4 = "session4";
+localStorage.setItem(session4, "Isso é uma sessão local");
+console.log(localStorage.getItem(session4));
+var x = 10;
+var y = 5;
+var z = 20;
+var soma1 = 0;
+function soma(x, y, z) {
+    return x + y + z;
+}
+console.log(soma(x, y, z));
--- main.ts
+++ main.ts
@@ -1,19 +1,33 @@
-// function listaReversa <T> (lista : Array<T>): Array<T> {
-//     let listaReversa: T[] = [];
-//         for (let i = lista.length -1; i>=0 ;i--){
-//             listaReversa.push(lista[i]);
-//         }
-//     return listaReversa;
-// }
-// let nomes = ["Luiz", "Marcos", "Lucas"];
-// let numeros = [1,2,3,4,5];
+//Exemplo Function
+let x = 10;
+let y = 5;
+let z = 20 ;
+let soma1 = 0;
 
-// let nomesReversos = listaReversa<string>(nomes);
-// let numerosReversos = listaReversa<number>(numeros);
+function soma (x , y, z){
+    return x+y+z;
+}
 
-// console.log(nomesReversos);
-// console.log(numerosReversos);
+console.log(soma(x,y,z));
 
+//Exemplo loop lista
+function listaReversa <T> (lista : Array<T>): Array<T> {
+    let listaReversa: T[] = [];
+        for (let i = lista.length -1; i>=0 ;i--){
+            listaReversa.push(lista[i]);
+        }
+    return listaReversa;
+}
+let nomes = ["Luiz", "Marcos", "Lucas"];
+let numeros = [1,2,3,4,5];
+
+let nomesReversos = listaReversa<string>(nomes);
+let numerosReversos = listaReversa<number>(numeros);
+
+console.log(nomesReversos);
+console.log(numerosReversos);
+
+//Exemplo click
 class ClickHandler{
     constructor(){
         let main = document.getElementById("main");
@@ -30,16 +44,52 @@ class ClickHandler{
         event.stopPropagation();
     }
     static alertEventBtn1(): void{
-        alert("Clicked over btn1");
+        alert("Você escolheu Goku");
         event.stopPropagation();
     }
     static alertEventBtn2(): void{
-        alert("Clicked over btn2");
+        alert("Você escolheu Vegeta");
         event.stopPropagation();
     }
   
 }
 const clickHandler = new ClickHandler();
+
+
+//     let button1 = document.querySelector("#btn1");
+//     let button2 = document.querySelector("#btn2");
+
+//     console.log(button1);
+//     console.log(button2);
+
+// var main2 = document.getElementById("main");
+// main2.innerHTML = "Alo"
+
+// console.log(main2);
+
+
+//Exemplos Storage
+let session1 = "session1";
+let session2 = "session2";
+let session3 = "session3";
+sessionStorage.setItem(session1, "Luiz");
+sessionStorage.setItem(session2, "Gustavo");
+sessionStorage.setItem(session3, "Borges Bosco");
+console.log(sessionStorage.getItem(session1));
+console.log(sessionStorage.getItem(session2));
+console.log(sessionStorage.getItem(session3));
+
+console.log("------------------------------------");
+
+for (let i = 0; i < sessionStorage.length; i++){
+    console.log(sessionStorage.getItem(<string>sessionStorage.key(i)));
+}
+
+console.log("------------------------------------");
+
+let session4 = "session4";
+localStorage.setItem(session4, "Isso é uma sessão local");
+console.log(localStorage.getItem(session4));
 
 
 
